40e0e01bfd41d186523d145664ee9ae61f20cef9
Anastasiya Alekseeva <anastasiyaalexxx@gmail.com>
2026-06-10 15:54:45 +0300

    Правки #5

--- src/const.ts
+++ src/const.ts
@@ -13,8 +13,6 @@ export enum AuthorizationStatus {
 
 export const STAR_WIDTH_PERCENT = 20;
 
-export const URL_MARKER_DEFAULT =
-  'https://assets.htmlacademy.ru/content/intensive/javascript-1/demo/interactive-map/pin.svg';
+export const URL_MARKER_DEFAULT = '/img/pin.svg';
 
-export const URL_MARKER_CURRENT =
-  'https://assets.htmlacademy.ru/content/intensive/javascript-1/demo/interactive-map/main-pin.svg';
+export const URL_MARKER_CURRENT = '/img/pin-active.svg';
